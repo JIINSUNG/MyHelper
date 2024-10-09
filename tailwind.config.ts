21e3d8d231fa3b6864042437c3952133157ec67f
JIINSUNG <crm03008@gmail.com>
2024-10-09 22:53:28 +0900

    chore : tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/widgets/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/entities/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/shared/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
 		extend: {
