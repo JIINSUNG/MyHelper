fe50fa5be9bd8f139241bed88b7e77d06feea3ac
JIINSUNG <crm03008@gmail.com>
2024-10-09 22:48:36 +0900

    refactor : 테이블 형식으로 변경

--- src/app/entities/RecommendItemsList.tsx
+++ src/app/entities/RecommendItemsList.tsx
@@ -83,77 +83,88 @@ const RecommendItemsList: React.FC = () => {
 	};
 
 	return (
-		<section className="flex w-full justify-between max-md:flex-wrap border-b border-black">
-			<div className="flex w-full flex-col">
-				<div className="flex items-center justify-center gap-2 border-b border-black p-4 h-[70px]">
-					<p>구매품목</p>
-					<button
-						className={twMerge(
-							'border border-black rounded-full bg-blue-300 size-[30px]',
-							editMode && 'bg-red-300 animate-pulse',
-						)}
-						onClick={() => setEditMode((mode) => !mode)}
-					>
-						{editMode ? '-' : '+'}
-					</button>
-				</div>
-				<div className="flex flex-col">
-					<div className="flex justify-between items-center px-2 text-center">
-						<p className="w-1/3">물품명</p>
-						<p className="w-1/3">가격</p>
-						<p className="w-1/3">수량</p>
-					</div>
-					<div className="flex mx-auto">
-						<button
-							className="border border-black px-2 py-1 rounded-md bg-blue-300"
-							onClick={handleCalculate}
-						>
-							계산하기
-						</button>
-					</div>
-					{itemList.map((item, index) => (
-						<div
-							key={item.name || index}
-							className="flex justify-between items-center px-2 text-center cursor-pointer"
-							onClick={() => {
-								if (confirm('삭제하시겠습니까?')) {
-									setItemList((prev) =>
-										prev.filter((i) => i.name !== item.name),
-									);
-								}
-							}}
-						>
-							<p className="w-1/3">{item.name}</p>
-							<p className="w-1/3">{item.price}</p>
-							<p className="w-1/3">{item.quantity}</p>
-						</div>
-					))}
-				</div>
-			</div>
-			<div className="md:border-l max-md:border-t max-md:w-full border-black"></div>
-			<div className="flex w-full flex-col">
-				<div className="flex items-center justify-center gap-2 border-b border-black p-4 h-[70px]">
-					<p>추천품목</p>
-				</div>
-				<div className="flex flex-col gap-2 p-4">
-					{recommendList.map((item, idx) => (
-						<div
-							className="flex w-full max-md:items-center flex-col gap-1 border-b border-black"
-							key={idx}
-						>
-							<p>추천 조합 {idx + 1}</p>
-							<div className="flex gap-2">
-								{item.pairs.map((i, subIdx) => (
-									<p key={`${i.name}-${subIdx}`}>
-										{i.name} {i.quantity}
-									</p>
-								))}
+		<section className="flex w-full justify-between max-md:flex-wrap">
+			<table className="table-auto w-full border-collapse">
+				<thead>
+					<tr className="flex justify-between w-full border-b border-black">
+						<th className="flex items-center justify-center gap-2 w-1/2 py-2">
+							<p>구매품목</p>
+							<button
+								className={twMerge(
+									'rounded-full bg-blue-300 w-8 h-8',
+									editMode && 'bg-red-300 animate-pulse',
+								)}
+								onClick={() => setEditMode((mode) => !mode)}
+							>
+								{editMode ? '-' : '+'}
+							</button>
+						</th>
+						<th className="flex items-center justify-center border-l border-black gap-2 w-1/2 py-2">
+							추천품목
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr className="flex justify-between w-full  border-b border-black">
+						<td className="flex w-full ">
+							<div className="flex w-full justify-between items-center px-2 text-center">
+								<p className="w-1/3">물품명</p>
+								<p className="w-1/3">가격</p>
+								<p className="w-1/3">수량</p>
 							</div>
-							<p>가격 : {item.total}</p>
-						</div>
-					))}
-				</div>
-			</div>
+						</td>
+						<td className="flex w-full border-l border-black">
+							<div className="flex w-full justify-center py-2">
+								<button
+									className="px-2 py-1 rounded-md bg-blue-300"
+									onClick={handleCalculate}
+								>
+									계산하기
+								</button>
+							</div>
+						</td>
+					</tr>
+					<tr className="flex justify-between w-full">
+						<td className="flex flex-col w-full">
+							{itemList.map((item, index) => (
+								<div
+									key={item.name || index}
+									className="flex w-full justify-between items-center px-2 text-center cursor-pointer"
+									onClick={() => {
+										if (confirm('삭제하시겠습니까?')) {
+											setItemList((prev) =>
+												prev.filter((i) => i.name !== item.name),
+											);
+										}
+									}}
+								>
+									<p className="w-1/3">{item.name}</p>
+									<p className="w-1/3">{item.price}</p>
+									<p className="w-1/3">{item.quantity}</p>
+								</div>
+							))}
+						</td>
+						<td className="flex flex-col w-full border-l border-black">
+							{recommendList.map((item, idx) => (
+								<div
+									className="flex w-full max-md:items-center flex-col gap-1 p-2"
+									key={idx}
+								>
+									<p>추천 조합 {idx + 1}</p>
+									<div className="flex gap-2">
+										{item.pairs.map((i, subIdx) => (
+											<p key={`${i.name}-${subIdx}`}>
+												{i.name} {i.quantity}
+											</p>
+										))}
+									</div>
+									<p>가격 : {item.total}</p>
+								</div>
+							))}
+						</td>
+					</tr>
+				</tbody>
+			</table>
 			{editMode && (
 				<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
 					<form
@@ -162,19 +173,19 @@ const RecommendItemsList: React.FC = () => {
 					>
 						<input
 							type="text"
-							className="border border-gray-300 rounded-md px-2 mb-2 w-full"
+							className="rounded-md border border-black px-2 py-4 mb-2 w-full"
 							placeholder="물품명"
 							name="name"
 						/>
 						<input
 							type="number"
-							className="border border-gray-300 rounded-md px-2 mb-2 w-full"
+							className="rounded-md border border-black px-2 py-4 mb-2 w-full"
 							placeholder="물품가격"
 							name="price"
 						/>
 						<input
 							type="number"
-							className="border border-gray-300 rounded-md px-2 mb-2 w-full"
+							className="rounded-md border border-black px-2 py-4 mb-2 w-full"
 							placeholder="물품 개수"
 							name="quantity"
 						/>
