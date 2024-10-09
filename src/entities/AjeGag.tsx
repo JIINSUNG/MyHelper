2eba306944dc8241ab35a9acfbb6b3369fad0bea
JIINSUNG <crm03008@gmail.com>
2024-10-10 01:14:57 +0900

    delete : 미사용 훅 삭제

--- src/entities/AjeGag.tsx
+++ src/entities/AjeGag.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import data from '@/shared/ajegag.json';
 
 const AjeGag: React.FC = () => {
