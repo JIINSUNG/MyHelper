c68e8ead5d6c4777137ff0f364fa5bc67b321b40
JIINSUNG <crm03008@gmail.com>
2024-10-09 23:40:30 +0900

    feat : 할인율(가격) 계산기

---
+++ src/app/discount/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DiscountCalculator } from '@/entities';
+
+const page: React.FC = () => {
+	return (
+		<main className="flex flex-col w-full items-center gap-4">
+			<h1 className="text-2xl font-bold">할인 가격(율) 계산기</h1>
+			<DiscountCalculator />
+		</main>
+	);
+};
+
+export default page;
