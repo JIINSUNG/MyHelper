fc9c494839db963c8e373020422a8ad18516a4ca
JIINSUNG <crm03008@gmail.com>
2024-10-09 22:55:02 +0900

    refactor : 폴더 위치 변경에 따른 import 변경

--- src/app/pages/GifticonPage.tsx
+++ src/app/pages/GifticonPage.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
-import { GifticonCalculator } from '@/app/entities';
-import { RecommendItemsList } from '@/app/entities';
+import { GifticonCalculator } from '@/entities';
+import { RecommendItemsList } from '@/entities';
 import { PriceContextProvider } from '@/app/context/PriceContext';
 
 const GifticonPage: React.FC = () => {
