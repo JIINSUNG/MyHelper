3d4efd2068e5bade66d4d20e92697d0ffa556f3e
JIINSUNG <crm03008@gmail.com>
2024-10-10 18:34:39 +0900

    rename : export 추가

---
+++ src/shared/component/index.ts
@@ -0,0 +1,4 @@
+export { default as Card } from './Card';
+export { default as Input } from './Input';
+export { default as InputModal } from './InputModal';
+export { default as MenuButton } from './MenuButton';
