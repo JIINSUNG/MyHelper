b1c9c678efb558ed7dd2a58e3dd9651d83d648a6
JIINSUNG <crm03008@gmail.com>
2024-10-10 18:31:46 +0900

    feat : Modal 컴포넌트 추가

---
+++ src/shared/component/InputModal.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode } from 'react';
+
+type ModalProps = {
+	isOpen: boolean;
+	onClose: () => void;
+	onSubmit?: (event: React.FormEvent<HTMLFormElement>) => void;
+	children: ReactNode;
+	label: string;
+};
+
+const InputModal: React.FC<ModalProps> = ({
+	isOpen,
+	onClose,
+	onSubmit,
+	children,
+	label,
+}) => {
+	if (!isOpen) return null;
+
+	return (
+		<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+			<div className="bg-white p-10 rounded-md shadow-md w-[400px]">
+				<h2 className="font-bold text-xl text-center pb-4">{label}</h2>
+				<form onSubmit={onSubmit}>
+					{children}
+					<div className="flex gap-2 mt-4">
+						<button
+							className="bg-blue-500 text-white rounded-md px-4 py-2"
+							type="submit"
+						>
+							Submit
+						</button>
+						<button
+							className="bg-red-500 text-white rounded-md px-4 py-2"
+							type="button"
+							onClick={onClose}
+						>
+							Cancel
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default InputModal;
