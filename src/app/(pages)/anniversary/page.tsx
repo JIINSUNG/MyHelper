f6823631733cdf503787917574a3d60b517767da
JIINSUNG <crm03008@gmail.com>
2024-10-12 11:10:06 +0900

    feat : 디데이 카운터 추가

---
+++ src/app/(pages)/anniversary/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AnniversaryPage from '@/pages/anniversary/AnniversaryPage';
+
+const page: React.FC = () => {
+	return <AnniversaryPage />;
+};
+
+export default page;
