d58d74486361a89537ef1e93bc62afb288ea3755
JIINSUNG <crm03008@gmail.com>
2024-10-14 13:45:21 +0900

    style : 모달창 스타일 수정

--- src/shared/component/InputModal.tsx
+++ src/shared/component/InputModal.tsx
@@ -21,21 +21,21 @@ const InputModal: React.FC<ModalProps> = ({
 		<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
 			<div className="bg-white p-10 rounded-md shadow-md w-[400px]">
 				<h2 className="font-bold text-xl text-center pb-4">{label}</h2>
-				<form className="flex flex-col" onSubmit={onSubmit}>
+				<form className="flex flex-col gap-2" onSubmit={onSubmit}>
 					{children}
 					<div className="flex gap-2 mt-4">
 						<button
 							className="bg-blue-500 text-white rounded-md px-4 py-2"
 							type="submit"
 						>
-							Submit
+							제출
 						</button>
 						<button
 							className="bg-red-500 text-white rounded-md px-4 py-2"
 							type="button"
 							onClick={onClose}
 						>
-							Cancel
+							취소
 						</button>
 					</div>
 				</form>
