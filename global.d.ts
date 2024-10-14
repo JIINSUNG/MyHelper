f94649ca76bdb833d59a734373195898ab94f52c
JIINSUNG <crm03008@gmail.com>
2024-10-14 13:41:46 +0900

    modify : API 키 유출 방지를 위한 환경변수 private 화

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,7 @@
 declare namespace NodeJS {
 	interface ProcessEnv {
-		NEXT_PUBLIC_TMDB_API_KEY: string;
 		TMDB_API_KEY: string;
-		NEXT_PUBLIC_TMDB_BASE_URL: string;
+		TMDB_BASE_URL: string;
 		STREAMING_API_KEY: string;
 		STREAMING_BASE_URL: string;
 		STREAMING_API_HOST: string;
