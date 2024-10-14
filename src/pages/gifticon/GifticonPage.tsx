93ec78f8aaec6ea3058fb8ec71b41ca578c70e0f
JIINSUNG <crm03008@gmail.com>
2024-10-14 15:15:49 +0900

    add : 헬퍼 제목 추가

--- src/pages/gifticon/GifticonPage.tsx
+++ src/pages/gifticon/GifticonPage.tsx
@@ -5,7 +5,8 @@ import { PriceContextProvider } from '@/app/context/PriceContext';
 const GifticonPage: React.FC = () => {
 	return (
 		<PriceContextProvider>
-			<main className="flex flex-col w-full md:p-4 whitespace-nowrap">
+			<main className="flex flex-col w-full md:p-4 items-center whitespace-nowrap">
+				<h1 className="text-2xl font-bold mb-4">기프티콘 헬퍼</h1>
 				<GifticonCalculator />
 				<RecommendItemsList />
 			</main>
