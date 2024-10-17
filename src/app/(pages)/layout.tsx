ebe00d16618af50c011ce1911811a8e342fc21dc
JIINSUNG <crm03008@gmail.com>
2024-10-17 15:28:46 +0900

    remove : 불필요한 문자 제거

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 		<html lang="ko">
 			<body className="min-w-[360px] font-pretendard flex flex-col w-full min-h-dvh bg-gray-100">
 				<Header />
-				<MenuSelection />`
+				<MenuSelection />
 				<ChannelTalk />
 				<section className="max-md:p-2 md:p-6 gap-4 flex flex-1 w-full md:mx-auto max-w-screen-xl">
 					{children}
