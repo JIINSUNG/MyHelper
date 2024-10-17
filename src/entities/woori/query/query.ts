8e689f708deca5243a6f7c17f197b56b00a5f501
JIINSUNG <crm03008@gmail.com>
2024-10-18 02:54:15 +0900

    fix : 객체 형태에서 배열 형태로 변경

--- src/entities/woori/query/query.ts
+++ src/entities/woori/query/query.ts
@@ -2,26 +2,18 @@ import { useQuery } from '@tanstack/react-query';
 import { getData } from '@/entities/woori/api/api';
 import { itemType } from '@/entities/woori';
 
-type searchType = {
-	brand: '애슐리' | '아웃백';
-	region: string;
-	name: string;
-};
-
 export const useGetWoori = (
 	brand: '애슐리' | '아웃백',
 	region: string,
 	name: string,
 ) => {
-	return useQuery<itemType[], Error, itemType[], [_1: string, _2: searchType]>({
-		queryKey: [
-			'Woori',
-			{
-				brand,
-				region,
-				name,
-			},
-		],
+	return useQuery<
+		itemType[],
+		Error,
+		itemType[],
+		[_1: string, _2: string, _3: string, _4: string]
+	>({
+		queryKey: ['Woori', brand, region, name],
 		queryFn: () => getData(brand, region, name),
 		staleTime: 1000 * 5 * 3600,
 	});
