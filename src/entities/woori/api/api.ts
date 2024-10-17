915c926e5e82bde3e14192a732a45f63d915dc1f
JIINSUNG <crm03008@gmail.com>
2024-10-18 02:50:52 +0900

    fix : Base URL 변경

--- src/entities/woori/api/api.ts
+++ src/entities/woori/api/api.ts
@@ -1,28 +1,17 @@
-import Swal from 'sweetalert2';
-
 export const getData = async (brand: string, region: string, name: string) => {
 	const searchParams = new URLSearchParams();
 	if (brand) searchParams.set('brand', brand);
 	if (region) searchParams.set('region', region);
 	if (name) searchParams.set('name', name);
 
-	try {
-		const response = await fetch(
-			process.env.URL + `/api/woori?${searchParams.toString()}`,
-			{
-				next: {
-					tags: ['Woori', brand, region, name],
-				},
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_BASE_URL}/api/woori?${searchParams.toString()}`,
+		{
+			next: {
+				tags: ['Woori', brand, region, name],
 			},
-		).then((response) => response.json());
+		},
+	).then((response) => response.json());
 
-		return response.data;
-	} catch (e) {
-		Swal.fire({
-			icon: 'error',
-			title: '에러',
-			text: '에러가 발생하였습니다',
-		});
-		console.error(e);
-	}
+	return response.data;
 };
