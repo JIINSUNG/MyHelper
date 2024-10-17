35d8f25e2b3a8b416002005a6fd429ee7ab5680d
JIINSUNG <crm03008@gmail.com>
2024-10-18 02:51:44 +0900

    fix : props 이름 명확화

--- src/features/woori/components/BrandCard.tsx
+++ src/features/woori/components/BrandCard.tsx
@@ -3,33 +3,35 @@ import React, { useState } from 'react';
 import { itemType } from '@/entities/woori';
 
 export type BrandCardProps = {
-	data: itemType;
+	props: itemType;
 };
 
-const BrandCard: React.FC<BrandCardProps> = ({ data }) => {
+const BrandCard: React.FC<BrandCardProps> = ({ props }) => {
 	const [isDetail, setIsDetail] = useState(false);
+
 	return (
 		<div
 			className="flex flex-col w-full"
 			onClick={() => setIsDetail((prev) => !prev)}
 		>
 			<div className="p-2 bg-white flex flex-col w-full">
-				<div>{data.name}</div>
-				<div>브랜드 : {data.brand}</div>
+				<div>{props.name}</div>
+				<div>브랜드 : {props.brand}</div>
 				<div>
 					할인 가능 여부 :&nbsp;
-					{data.discount
+					{props.discount
 						? '가능'
-						: data.discount === false
+						: props.discount === false
 							? '불가'
 							: '확인 불가'}
 				</div>
 			</div>
+
 			{isDetail && (
 				<div className="p-2 flex flex-col w-full bg-gray-200">
-					<span>주소 : {data.address}</span>
-					<span>연락처 : {data.tel}</span>
-					{data.etc && <span>특이사항 : {data.etc}</span>}
+					<span>주소 : {props.address}</span>
+					<span>연락처 : {props.tel}</span>
+					{props.etc && <span>특이사항 : {props.etc}</span>}
 				</div>
 			)}
 		</div>
