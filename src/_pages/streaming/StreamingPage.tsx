902f5359e7743415ac5f9045aaebc088c692051c
JIINSUNG <crm03008@gmail.com>
2024-10-19 21:19:20 +0900

    add : Context API 추가

--- src/_pages/streaming/StreamingPage.tsx
+++ src/_pages/streaming/StreamingPage.tsx
@@ -1,15 +1,16 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { MovieSelector, StreamingList } from '@/features/streaming';
+import { MovieContextProvider } from '@/app/context/MovieContext';
 
 const page = () => {
 	return (
 		<main className="flex flex-col w-full items-center gap-4">
-			<h1 className="text-2xl font-bold">스트리밍 헬퍼</h1>
-			<MovieSelector />
-			<div className="border-[1px] border-b w-full border-black" />
-			<Suspense fallback={<div>Loading...</div>}>
+			<MovieContextProvider>
+				<h1 className="text-2xl font-bold">스트리밍 헬퍼</h1>
+				<MovieSelector />
+				<div className="border-[1px] border-b w-full border-black" />
 				<StreamingList />
-			</Suspense>
+			</MovieContextProvider>
 		</main>
 	);
 };
