122b98a77793791785741fdd34f30863b0c3d0bd
JIINSUNG <crm03008@gmail.com>
2024-10-19 21:18:39 +0900

    add : 스켈레톤 로딩 UI 추가

--- src/features/woori/index.ts
+++ src/features/woori/index.ts
@@ -3,3 +3,4 @@ export { default as RegionSelector } from './components/RegionSelector';
 export { default as BrandList } from './components/BrandList';
 export { default as BrandCard } from './components/BrandCard';
 export { default as NameSelector } from './components/NameSelector';
+export { default as BrandLoadingCard } from './components/BrandLoadingCard';
