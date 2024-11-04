4e578312ece917c8fd638ef7a4e85e9cd221f24c
JIINSUNG <crm03008@gmail.com>
2024-11-04 22:40:59 +0900

    feat : 혜택 헬퍼 관련 타입 추가

--- src/entities/benefit/type/type.ts
+++ src/entities/benefit/type/type.ts
@@ -7,3 +7,33 @@ export type MainCategoryType = {
 export type SubCategoryType = MainCategoryType & {
 	pid: number;
 };
+
+/**
+ * 데이터 원본 타입
+ */
+export type BenefitDataType = {
+	category1: string;
+	category2: string;
+	category3: string;
+	brandId: number;
+	id: number;
+	image: string;
+	name: string;
+	benefitTitle: string;
+	benefitDescription: string;
+	benefitImage: string | null;
+	expiration: string;
+	condition: string;
+	conditionDescription: string;
+	HowGet: string;
+	LINK: string;
+	ETC: string | null;
+};
+
+export type BenefitType = {
+	category3: string;
+	image: string;
+	name: string;
+	id: number;
+	brandId: number;
+};
