eeac7c9b9349a1e023abb4a327cdf65f6e3e25fd
JIINSUNG <crm03008@gmail.com>
2024-11-04 22:53:20 +0900

    feat : 혜택 헬퍼 관련 쿼리 훅 추가

--- src/entities/benefit/query/query.ts
+++ src/entities/benefit/query/query.ts
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import {
+	getBenefitBrandList,
+	getBenefitDetail,
 	getBenefitList,
 	getMainCategory,
 	getSubCategory,
 } from '@/entities/benefit/api/api';
 import {
+	BenefitDataType,
+	BenefitType,
 	MainCategoryType,
 	SubCategoryType,
 } from '@/entities/benefit/type/type';
@@ -32,13 +36,39 @@ export const useGetSubCategory = (pid: string) => {
 
 export const useGetBenefitList = (pid: string, sid: string) => {
 	return useQuery<
-		SubCategoryType[],
+		BenefitType[],
 		Error,
-		SubCategoryType[],
+		BenefitType[],
 		[_1: string, _2: string, _3: string]
 	>({
-		queryKey: ['benefitSubCategory', pid, sid],
+		queryKey: ['benefitList', pid, sid],
 		queryFn: () => getBenefitList(pid, sid),
+		staleTime: 1000 * 60 * 5,
+	});
+};
+
+export const useGetBenefitBrandList = (pid: string, brandId: string) => {
+	return useQuery<
+		BenefitDataType[],
+		Error,
+		BenefitDataType[],
+		[_1: string, _2: string, _3: string]
+	>({
+		queryKey: ['benefitBrand', pid, brandId],
+		queryFn: () => getBenefitBrandList(pid, brandId),
+		staleTime: 1000 * 60 * 5,
+	});
+};
+
+export const useGetBenefitDetail = (pid: string, benefitId: string) => {
+	return useQuery<
+		BenefitDataType,
+		Error,
+		BenefitDataType,
+		[_1: string, _2: string, _3: string]
+	>({
+		queryKey: ['benefitDetail', pid, benefitId],
+		queryFn: () => getBenefitDetail(pid, benefitId),
 		staleTime: 1000 * 60 * 5,
 	});
 };
