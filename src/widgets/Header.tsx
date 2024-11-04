997294f3d0cfbcdb1de82a9df590c51342e5e67a
JIINSUNG <crm03008@gmail.com>
2024-11-05 01:01:05 +0900

    feat : 헤더 로고 클릭 시 홈화면으로 이동

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 export type HeaderProps = {
 	className?: string;
 };
@@ -8,13 +9,15 @@ const Header: React.FC<HeaderProps> = () => {
 	return (
 		<header className="flex justify-center p-10 max-md:p-5 bg-gray-200">
 			<div className="relative w-[200px] h-[40px] animate-slideUp">
-				<Image
-					src="/Logo/Logo2.png"
-					width={200}
-					height={40}
-					alt="Logo"
-					className="w-full h-full object-cover"
-				/>
+				<Link href={process.env.NEXT_PUBLIC_BASE_URL}>
+					<Image
+						src="/Logo/Logo2.png"
+						width={200}
+						height={40}
+						alt="Logo"
+						className="w-full h-full object-cover"
+					/>
+				</Link>
 			</div>
 		</header>
 	);
